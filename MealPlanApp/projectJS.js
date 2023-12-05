eca4eb00a8fc0b4371b649ed445baa7fd47e06c1
ahwilson3 <113067417+ahwilson27615@users.noreply.github.com>
2023-12-04 20:23:39 -0500

    v2

--- MealPlanApp/projectJS.js
+++ MealPlanApp/projectJS.js
@@ -4,14 +4,46 @@ function togglePersonalInfoDisplay(displayValue) {
     document.getElementById('personalInfo').style.display = displayValue;
   }
   
-  document.getElementById('mondayButton').addEventListener('click', function() {
+  document.getElementById('mondayButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form behavior
     showDay('monday');
-    togglePersonalInfoDisplay('none'); // Hide personal info when Monday is clicked
+    // togglePersonalInfoDisplay('none'); // Hide personal info when Monday is clicked
   });
   
-  document.getElementById('tuesdayButton').addEventListener('click', function() {
+  document.getElementById('tuesdayButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form behavior
     showDay('tuesday');
-    togglePersonalInfoDisplay('none'); // Hide personal info when Tuesday is clicked
+    // togglePersonalInfoDisplay('none'); // Hide personal info when Tuesday is clicked
+  });
+
+  document.getElementById('wednesdayButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form behavior
+    showDay('wednesday');
+    // togglePersonalInfoDisplay('none'); // Hide personal info when Tuesday is clicked
+  });
+
+  document.getElementById('thursdayButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form behavior
+    showDay('thursday');
+    // togglePersonalInfoDisplay('none'); // Hide personal info when Tuesday is clicked
+  });
+
+  document.getElementById('fridayButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form behavior
+    showDay('friday');
+    // togglePersonalInfoDisplay('none'); // Hide personal info when Tuesday is clicked
+  });
+  
+  document.getElementById('saturdayButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form behavior
+    showDay('saturday');
+    // togglePersonalInfoDisplay('none'); // Hide personal info when Tuesday is clicked
+  });
+
+  document.getElementById('sundayButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form behavior
+    showDay('sunday');
+    // togglePersonalInfoDisplay('none'); // Hide personal info when Tuesday is clicked
   });
   
   // Add similar event listeners for other days
@@ -22,39 +54,36 @@ function togglePersonalInfoDisplay(displayValue) {
     goal: ''
   };
   
-  function savePersonalInfo() {
-    personalInfo.name = document.getElementById('name').value;
-    personalInfo.email = document.getElementById('email').value;
-    personalInfo.goal = document.getElementById('goal').value;
-  }
-  
-  function showDay(day) {
-    const days = ['monday', 'tuesday', /*...add other days here*/];
-    const mealOptions = document.getElementById('mealOptions');
+  function showDay(selectedDay) {
+    const days = ['monday', 'tuesday', 'wednesday' , 'thursday' , 'friday' , 'saturday' , 'sunday'];
   
     days.forEach(day => {
       const dayOptions = document.getElementById(day + 'Options');
       if (dayOptions) dayOptions.style.display = 'none';
     });
   
-    const selectedDay = document.getElementById(day + 'Options');
-    if (selectedDay) selectedDay.style.display = 'block';
+    const selectedDayOptions = document.getElementById(selectedDay + 'Options');
+    if (selectedDayOptions) selectedDayOptions.style.display = 'block';
   
-    populateMealPlan(day);
+    // populateMealPlan(day);
   }
   
   function populateMealPlan(day) {
     const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+    personalInfo.name = document.getElementById('name').value;
+    personalInfo.email = document.getElementById('email').value;
+    personalInfo.goal = document.getElementById('goal').value;
+
     let mealPlanContent = `
       <h2>Weekly Meal Plan</h2>
-      <h3>Name: ${personalInfo.name}</h3>
+      <p>Name: ${personalInfo.name}</p>
       <p>Email: ${personalInfo.email}</p>
       <p>Weekly Goal: ${personalInfo.goal}</p>
     `;
   
     days.forEach(day => {
       const dayName = day.charAt(0).toUpperCase() + day.slice(1);
-      // Adjust the IDs to match your input fields for each day
       const breakfast = document.getElementById(day + 'Breakfast').value;
       const snack1 = document.getElementById(day + 'Snack1').value;
       const lunch = document.getElementById(day + 'Lunch').value;
@@ -70,7 +99,7 @@ function togglePersonalInfoDisplay(displayValue) {
         <p>Dinner: ${dinner}</p>
       `;
     });
-  
+
     const mealPlan = document.getElementById('mealOptions');
     mealPlan.innerHTML = mealPlanContent;
   }
